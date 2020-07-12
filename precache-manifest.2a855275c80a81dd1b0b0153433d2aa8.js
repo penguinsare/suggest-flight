0e781346c7823c9155190323440b2e81bf2ff1ce
Banana1 <nqmammail@shop.com>
2020-07-12 20:41:03 +0300

    Updates

---
+++ precache-manifest.2a855275c80a81dd1b0b0153433d2aa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c264432b1764c10ef136a4b9c3b3b7",
+    "url": "/suggest-flight/index.html"
+  },
+  {
+    "revision": "9634271c85dc4f92e24a",
+    "url": "/suggest-flight/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "d7f511efee2a6c65f37f",
+    "url": "/suggest-flight/static/css/main.3acb7072.chunk.css"
+  },
+  {
+    "revision": "9634271c85dc4f92e24a",
+    "url": "/suggest-flight/static/js/2.2f2f5cd6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/suggest-flight/static/js/2.2f2f5cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f511efee2a6c65f37f",
+    "url": "/suggest-flight/static/js/main.bea90e8e.chunk.js"
+  },
+  {
+    "revision": "5a4264d5c714caa27839",
+    "url": "/suggest-flight/static/js/runtime-main.44177c92.js"
+  }
+]);
